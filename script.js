dfc181f683b8bc3b1e31d1385b9b0a9552f221c1
Talissa <liayai@ya.ru>
2025-04-15 21:15:17 +0300

    feat: add burger menu

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const header = document.querySelector('.header');
+const burgerMenu = document.querySelector('.burger-menu');
+const headerNav = document.querySelector('.header-nav');
+const overlay = document.querySelector('.overlay');
 const scrollStart = 100; 
 
 window.addEventListener('scroll', () => {
@@ -6,5 +9,27 @@ window.addEventListener('scroll', () => {
     header.classList.add('scrolled');
   } else {
     header.classList.remove('scrolled');
+  }
+});
+
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('active');
+  headerNav.classList.toggle('show');
+});
+
+headerNav.addEventListener('click', () => {
+  headerNav.classList.remove('show');
+  burgerMenu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  headerNav.classList.remove('show');
+  burgerMenu.classList.remove('active');
+});
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    headerNav.classList.remove('show');
+    burgerMenu.classList.remove('active');
   }
 });
